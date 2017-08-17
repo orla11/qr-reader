9585de768de12b99f1007040aa14e9ed51d11e4b
orla11 <orlandif.mail@gmail.com>
2017-08-17 15:51:04 +0200

    first commit

--- server.js
+++ server.js
@@ -21,17 +21,17 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+//app.use(forceSSL());
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join('/dist/index.html'));
 });
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist'));
 
 // Start the app by listening on the default
 // Heroku port
